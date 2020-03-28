e5bc6b40ebc8aac363c8dbfe8daf086be4f3bab1
Erik Stroud <estroud1991@gmail.com>
2020-03-28 19:51:58 -0400

    Set up

--- app/src/main/java/com/stroud/hawkbay/MainActivity.java
+++ app/src/main/java/com/stroud/hawkbay/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.myapplication;
+package com.stroud.hawkbay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.stroud.hawkbay.R;
 
 public class MainActivity extends AppCompatActivity {
 
